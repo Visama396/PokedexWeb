26cd0aeb0f85111df893d66cab885cc3676d4009
Victor <visama396@gmail.com>
2024-02-05 03:19:38 +0100

    adding netlify and building site for production

--- pokedex/astro.config.mjs
+++ pokedex/astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import netlify from '@astrojs/netlify'
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server'
-});
+  output: 'hybrid',
+  adapter: netlify()
+})
