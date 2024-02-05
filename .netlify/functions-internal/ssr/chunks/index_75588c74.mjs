83ac7707fdf476370f89d4979b956926e1e78f06
Victor <visama396@gmail.com>
2024-02-05 11:48:42 +0100

    update

--- .netlify/functions-internal/ssr/chunks/index_75588c74.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./pages/index_a50c2b85.mjs');
-
-export { page };
--- .netlify/functions-internal/ssr/entry.mjs
+++ .netlify/functions-internal/ssr/entry.mjs
@@ -1,14 +1,14 @@
 import * as adapter from '@astrojs/netlify/ssr-function.js';
 import { renderers } from './renderers.mjs';
-import { manifest } from './manifest_aea7f810.mjs';
+import { manifest } from './manifest_e4439ef4.mjs';
 import './chunks/astro_ab4bf765.mjs';
 import './chunks/pages/generic_7f9eef86.mjs';
 import './chunks/astro-assets-services_dc74f136.mjs';
 
 const _page0  = () => import('./chunks/generic_12ad4597.mjs');
-const _page1  = () => import('./chunks/index_75588c74.mjs');
+const _page1  = () => import('./chunks/index_46a4f056.mjs');
 const _page2  = () => import('./chunks/404_ea62887f.mjs');
-const _page3  = () => import('./chunks/_pokemon__4674112a.mjs');const pageMap = new Map([["node_modules/.pnpm/astro@3.4.4_typescript@5.2.2/node_modules/astro/dist/assets/endpoint/generic.js", _page0],["src/pages/index.astro", _page1],["src/pages/404.astro", _page2],["src/pages/[pokemon].astro", _page3]]);
+const _page3  = () => import('./chunks/_pokemon__7c5ffaf0.mjs');const pageMap = new Map([["node_modules/.pnpm/astro@3.4.4_typescript@5.2.2/node_modules/astro/dist/assets/endpoint/generic.js", _page0],["src/pages/index.astro", _page1],["src/pages/404.astro", _page2],["src/pages/[pokemon].astro", _page3]]);
 const _manifest = Object.assign(manifest, {
 	pageMap,
 	renderers,
