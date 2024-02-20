a73f651ea0e14981848b724759845ef2d2d30a83
Victor <visama396@gmail.com>
2024-02-20 21:40:11 +0100

    Added new UI updates

--- src/utils/capitalize.ts
+++ src/utils/capitalize.ts
@@ -1,1 +1,20 @@
 export const capitalize = ([first, ...rest]: string, lowerRest = false) => first.toUpperCase() + (lowerRest ? rest.join('').toLowerCase() : rest.join(''))
+
+export const backspaceAndCapitalize = (name : string) => {
+	let afterhyphen = false
+	let result = ""
+
+	for (const char of name) {
+		if (afterhyphen) {
+			result += char.toUpperCase()
+			afterhyphen = false
+		} else if (char === '-') {
+			result += ' '
+			afterhyphen = true
+		} else {
+			result += char
+		}
+	}
+
+	return capitalize(result)
+}
