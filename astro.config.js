774873f73b88f8b9b13ae0cc35428277bdb7db93
Victor <visama396@gmail.com>
2025-10-02 11:15:14 +0200

    Added react

--- astro.config.js
+++ astro.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 import tailwind from '@astrojs/tailwind';
 
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
   output: 'server',
   adapter: netlify()
 });
