ec62476240095c1d04f0fe8df893cc91c3a009bd
Victor <visama396@gmail.com>
2025-12-02 10:00:22 +0100

    added jsdocs

--- src/utils/capitalize.ts
+++ src/utils/capitalize.ts
@@ -1,8 +1,20 @@
+
+/**
+ *
+ * @param word The string to capitalize
+ * @param lowerRest whether to lowercase the rest of the string (default: false)
+ * @returns The capitalized string
+ */
 export const capitalize = ([first, ...rest]: string, lowerRest = false) => first.toUpperCase() + (lowerRest ? rest.join('').toLowerCase() : rest.join(''))
 
+/**
+ *
+ * @param word The string to capitalize
+ * @returns The capitalized string
+ */
 export const backspaceAndCapitalize = (name : string) => {
 	let afterhyphen = false
-	let result = ""
+	let result = ''
 
 	for (const char of name) {
 		if (afterhyphen) {
