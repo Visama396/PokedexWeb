359948cf109cac7ba34882f9cc89d2fc237dd0c0
Victor <visama396@gmail.com>
2025-12-02 15:21:02 +0100

    creating new section
    Creating Pokedle

---
+++ src/utils/getDailyPokemon.js
@@ -0,0 +1,14 @@
+export const getDailyPokemon = () => {
+	const today = new Date().toISOString().slice(0, 10)
+	const seed = today.replace(/-/g, '')
+
+	function seededRandom(s) {
+		const x = Math.sin(s) * 10000
+		return x - Math.floor(x)
+	}
+
+	const totalPokemon = 1025
+	const random = seededRandom(Number(seed))
+
+	return Math.floor(random * totalPokemon) + 1
+}
