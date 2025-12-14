a4b6dd35909229c0e540741e5e9c1cbc3aef4217
Victor <visama396@gmail.com>
2025-12-14 22:08:02 +0100

    update app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from 'react'
 
-export default function App() {
-	const [language, setLanguage] = useState('en')
+export default function App({ lang = 'en' }) {
+	const [language, setLanguage] = useState(lang)
 
 	return (
 		<main className='flex flex-col items-center justify-center'>
 			<h1>Pokedex</h1>
+			<h2>{language}</h2>
 		</main>
 	)
 }
