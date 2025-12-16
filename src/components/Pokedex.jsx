8061525ac97fcfc66386fa3a9727ad5b14171e03
Victor <visama396@gmail.com>
2025-12-16 12:00:17 +0100

    update dex2

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -6,18 +6,10 @@ export default function Pokedex({ lang = 'en', pokedex }) {
 	const [dex, setDex] = useState(pokedex)
 	const [language, setLanguage] = useState(lang)
 
-	/* useEffect(() => {
-		if (pokedex.length === 0) {
-			fetch('https://pokeapi.co/api/v2/pokemon/?limit=1025')
-				.then(response => response.json())
-				.then(data => setDex(data.results))
-		}
-	}, []) */
-
 	return (
-		<main className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-3 px-4' style={{ gridAutoRows: '1fr' }}>
-			{dex.map((pokemon, _) => (
-				<PokedexItem key={_} pokemonName={capitalize(pokemon.name)} pokemonUrl={pokemon.url} />
+		<main className='grid w-full grid-cols-2 xl:max-w-[70%] md:grid-cols-3 lg:grid-cols-5 gap-3 px-4' style={{ gridAutoRows: '1fr' }}>
+			{dex.map((entry) => (
+				<PokedexItem key={entry.entry_number} nationalEntry={entry.entry_number} pokemonName={capitalize(entry.pokemon_species.name)} pokemonUrl={entry.pokemon_species.url} />
 			))}
 		</main>
 	)
--- src/components/PokedexItem.jsx
+++ src/components/PokedexItem.jsx
@@ -1,20 +1,51 @@
 import { useState, useEffect } from 'react'
-import { getIdFromUrl } from '../utils/getIdFromUrl'
+import { getDexNumber } from '../utils/getDexNumber'
+import { capitalize, backspaceAndCapitalize } from '../utils/capitalize'
+import { translate } from '../utils/translate'
 
-export default function PokedexItem({ pokemonName, pokemonUrl }) {
+export default function PokedexItem({ nationalEntry, pokemonName, pokemonUrl }) {
 	const [name, setName] = useState(pokemonName)
 	const [url, setUrl] = useState(pokemonUrl)
-	const [id, setId] = useState(getIdFromUrl(pokemonUrl))
+	const [id, setId] = useState(0)
 	const [pokemon, setPokemon] = useState(null)
 	const [species, setSpecies] = useState(null)
 
 	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const res1 = await fetch(url)
+				const speciesData = await res1.json()
+				setSpecies(speciesData)
 
+				const varietyUrl = speciesData.varieties.find(vari => vari.is_default)?.pokemon.url
+
+				const res2 = await fetch(varietyUrl)
+				const pokemonData = await res2.json()
+				setPokemon(pokemonData)
+			} catch (error) {
+				console.error(error)
+			}
+		}
+
+		fetchData()
 	}, [])
 
-	return (
-		<a href={`/${name.toLowerCase()}`} className='flex flex-col justify-center items-center py-2 transition-all rounded-md w-full h-full text-white hover:scale-105 hover:shadow-2xl pokemon-card'>
-			{name}
+	return pokemon && (
+		<a
+			id={name}
+			href={`/${name.toLowerCase()}`}
+			className='flex flex-col justify-center items-center py-2 transition-all rounded-md w-full h-full text-white hover:scale-105 hover:shadow-2xl pokemon-card'
+			style={{ background: pokemon.types.length == 1 ? `var(--${pokemon.types[0].type.name})` : `linear-gradient(135deg, var(--${pokemon.types[0].type.name}), var(--${pokemon.types[1].type.name}))` }}>
+			<p className='text-center text-xl'>#{getDexNumber(nationalEntry)}</p>
+			<img className='object-cover mx-auto' src={pokemon.sprites.other.home.front_default} alt={`sprite ${name}`} />
+			<div className=''>
+				<h1 className='font-black text-center text-lg md:text-2xl pokemon'>{backspaceAndCapitalize(name)}</h1>
+				<p className='text-center font-bold text-sm'>
+					{
+						pokemon.types.map(typ => <span style={{ color: `var(--light-${typ.type.name})`, textShadow: '0px 0px 6px rgba(255, 255, 255, 0.4)' }}>{typ.type.name}</span>)
+					}
+				</p>
+			</div>
 		</a>
 	)
 }
